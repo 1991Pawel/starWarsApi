1c2d5e0d2bfbeccb47359ff6b16a9d8a6734375f
Pawel L <virtuozoo@o2.pl>
2021-05-08 16:48:01 +0200

    change display information in DetailsItem

--- src/components/DetailsItem/DetailsItem.tsx
+++ src/components/DetailsItem/DetailsItem.tsx
@@ -6,21 +6,35 @@ const DetailsItem = ({ data }: any) => {
     <div className={styles.listWrapper}>
       <ul className={styles.list}>
         <li className={styles.listItem}>Name: {data?.results[0]?.name}</li>
-        <li className={styles.listItem}>{data?.results[0]?.gender}</li>
+        {data?.results[0]?.gender && (
+          <li className={styles.listItem}>sex: {data?.results[0]?.gender}</li>
+        )}
+
         <li className={styles.listItem}>
           created: {data?.results[0]?.created}
         </li>
-        <li className={styles.listItem}>
-          population: {data?.results[0]?.population}
-        </li>
-        <li className={styles.listItem}>
-          terrain: {data?.results[0]?.terrain}
-        </li>
-        <li className={styles.listItem}>model: {data?.results[0]?.model}</li>
-        <li className={styles.listItem}>
-          passengers: {data?.results[0]?.passengers}
-        </li>
-        <li className={styles.listItem}>crew: {data?.results[0]?.crew}</li>
+
+        {data?.results[0]?.population && (
+          <li className={styles.listItem}>
+            population: {data?.results[0]?.population}
+          </li>
+        )}
+        {data?.results[0]?.terrain && (
+          <li className={styles.listItem}>
+            terrain: {data?.results[0]?.terrain}
+          </li>
+        )}
+        {data?.results[0]?.model && (
+          <li className={styles.listItem}>model: {data?.results[0]?.model}</li>
+        )}
+        {data?.results[0]?.passengers && (
+          <li className={styles.listItem}>
+            passengers: {data?.results[0]?.passengers}
+          </li>
+        )}
+        {data?.results[0]?.crew && (
+          <li className={styles.listItem}>crew: {data?.results[0]?.crew}</li>
+        )}
       </ul>
     </div>
   );
