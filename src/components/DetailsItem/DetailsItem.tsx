6ebb2ac2c232a7cc82ec19966cc911dc1a75b691
Pawel L <virtuozoo@o2.pl>
2021-05-08 17:00:55 +0200

    refactor DetailItem

--- src/components/DetailsItem/DetailsItem.tsx
+++ src/components/DetailsItem/DetailsItem.tsx
@@ -2,39 +2,30 @@ import React from 'react';
 import styles from '../DetailsItem/detailsItem.module.css';
 
 const DetailsItem = ({ data }: any) => {
+  const item = data?.results[0];
   return (
     <div className={styles.listWrapper}>
       <ul className={styles.list}>
-        <li className={styles.listItem}>Name: {data?.results[0]?.name}</li>
-        {data?.results[0]?.gender && (
-          <li className={styles.listItem}>sex: {data?.results[0]?.gender}</li>
+        <li className={styles.listItem}>Name: {item?.name}</li>
+        {item?.gender && (
+          <li className={styles.listItem}>sex: {item.gender}</li>
         )}
 
-        <li className={styles.listItem}>
-          created: {data?.results[0]?.created}
-        </li>
+        <li className={styles.listItem}>created: {item?.created}</li>
 
-        {data?.results[0]?.population && (
-          <li className={styles.listItem}>
-            population: {data?.results[0]?.population}
-          </li>
+        {item?.population && (
+          <li className={styles.listItem}>population: {item?.population}</li>
         )}
-        {data?.results[0]?.terrain && (
-          <li className={styles.listItem}>
-            terrain: {data?.results[0]?.terrain}
-          </li>
+        {item?.terrain && (
+          <li className={styles.listItem}>terrain: {item?.terrain}</li>
         )}
-        {data?.results[0]?.model && (
-          <li className={styles.listItem}>model: {data?.results[0]?.model}</li>
+        {item?.model && (
+          <li className={styles.listItem}>model: {item?.model}</li>
         )}
-        {data?.results[0]?.passengers && (
-          <li className={styles.listItem}>
-            passengers: {data?.results[0]?.passengers}
-          </li>
+        {item?.passengers && (
+          <li className={styles.listItem}>passengers: {item?.passengers}</li>
         )}
-        {data?.results[0]?.crew && (
-          <li className={styles.listItem}>crew: {data?.results[0]?.crew}</li>
-        )}
+        {item?.crew && <li className={styles.listItem}>crew: {item?.crew}</li>}
       </ul>
     </div>
   );
